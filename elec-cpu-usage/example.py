21241d756a07d75817e92ee911b48be0e939187d
George551556 <2869948036@qq.com>
2024-12-24 15:54:04 +0800

    增加了CPU利用率数据的转换； 分别用两个目录表示物管平台操作和CPU利用率；

---
+++ elec-cpu-usage/example.py
@@ -0,0 +1,81 @@
+import argparse
+import torch
+from deeplog              import DeepLog
+from deeplog.preprocessor import Preprocessor
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Run DeepLog model prediction.")
+    parser.add_argument(
+        "file_path",
+        type=str,
+        help="Relative path to the input txt file containing the dataset.",
+    )
+    args = parser.parse_args()
+
+    # Extract file path from arguments
+    file_path = args.file_path
+    ##############################################################################
+    #                                 Load data                                  #
+    ##############################################################################
+
+    # Create preprocessor for loading data
+    preprocessor = Preprocessor(
+        length  = 20,           # Extract sequences of 20 items
+        timeout = float('inf'), # Do not include a maximum allowed time between events
+    )
+
+    # Load data from txt file
+    X, y, label, mapping = preprocessor.text(
+        path = file_path,
+        mapping= {i: i for i in range(0, 99)}
+    )
+
+    ##############################################################################
+    #                                  DeepLog                                   #
+    ##############################################################################
+
+    # Create DeepLog object
+    deeplog = DeepLog(
+        input_size  = 100, # Number of different events to expect
+        hidden_size = 64 , # Hidden dimension, we suggest 64
+        output_size = 100, # Number of different events to expect
+    )
+
+    # Optionally cast data and DeepLog to cuda, if available
+    # deeplog = deeplog.to("cuda")
+    # X       = X      .to("cuda")
+    # y       = y      .to("cuda")
+
+    # Load model weights
+    model_path = './weight-cpu-usage.pth'
+    deeplog.load_state_dict(torch.load(model_path, map_location='cpu'))
+
+    # Predict using deeplog
+    y_pred, confidence = deeplog.predict(
+        X = X,
+        k = 3,
+    )
+
+    print('------------------------------------')
+    print('预测结果如下：(每行的第一个列表表示滑动窗口读入的真实数据，第二个列表为三个可能性依次降低的下一个事件ID)')
+    for i in range(len(y_pred.numpy())):
+        if i < 20:
+            continue # Skip predictions for the first 20 elements
+        isGood = False
+        if i == len(y_pred.numpy()) - 1:
+            print(X.numpy()[i], y_pred.numpy()[i])
+            continue
+        for j in range(len(y_pred.numpy()[i])):
+            if y_pred.numpy()[i][j] == X.numpy()[i + 1][19]:
+                # Prediction matches the true value
+                print(X.numpy()[i], y_pred.numpy()[i])
+                isGood = True
+                continue
+        if not isGood:
+            print(X.numpy()[i], y_pred.numpy()[i], '预测异常')
+
+    print('shape: ', y_pred.numpy().shape)
+
+if __name__ == "__main__":
+    main()
--- examples/elec-dataPro.go
+++ examples/elec-dataPro.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -24,17 +25,25 @@ var allOpCategories []string = []string{"com.fastbee.web.controller.system.SysDe
 }
 
 func main() {
-	hash = make(map[string]int)
-	for id, item := range allOpCategories {
-		hash[item] = id
+	args := os.Args[1:]
+	if len(args) < 2 || args[0] == "-h" {
+		fmt.Println("*****使用说明*****")
+		fmt.Println("参数1：待转换文件相对路径\n参数2：日志文件类型（1为物管平台操作日志，2为进程CPU使用率，3为****）")
+		fmt.Println("模板：./elec-dataPro [ 参数1 ] [ 参数2 ]")
+		fmt.Println("示例：./elec-dataPro ./IOT-manage.csv 1")
+		return
 	}
 
-	files, err := getCsvFiles()
-	if err != nil {
-		panic(err)
-	}
-	for _, item := range files {
-		getStateDigit(item, 3)
+	if args[1] == "1" {
+		hash = make(map[string]int)
+		for id, item := range allOpCategories {
+			hash[item] = id
+		}
+		getStateDigit(args[0], 3)
+	} else if args[1] == "2" {
+		getCPUusageStateDigit(args[0], 0)
+	} else {
+		fmt.Println("参数2 无效")
 	}
 }
 
@@ -58,10 +67,66 @@ func getCsvFiles() ([]string, error) {
 	return csvFiles, nil
 }
 
-// 根据传入的csv文件路径进行数据转换并写入同名的txt文件中，参数：（csv文件路径，目标数据列的位置）
+// 用于转换CPU使用率并输出到同名文件，参数：（csv文件路径，目标数据列的位置）
+func getCPUusageStateDigit(filepath string, targetidx int) {
+	if !isGoodFormat(filepath) {
+		log.Println(filepath, "错误的目标文件格式...")
+		return
+	}
+	clrTXT_file(filepath) //先清空目标的txt文件
+	sum := -1             //用于跳过第一行的表头
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	msg := ""
+	for {
+		line, err := reader.Read()
+		if sum == -1 {
+			sum++
+			continue
+		}
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		if targetidx >= len(line) {
+			panic("out of index...")
+		}
+		sum++
+		//对浮点百分比数据转换为整数**********************
+		dotId := 1
+		for i := 0; i < len(line[targetidx]); i++ {
+			if line[targetidx][i] == '.' {
+				dotId = i
+				break
+			}
+		}
+		tmpDGT := line[targetidx][:dotId]
+		tmp_1 := convertIn100(tmpDGT)
+		//对浮点百分比数据转换为整数**********************
+		msg += fmt.Sprintf("%v ", tmp_1)
+		if sum%50 == 0 {
+			toTXT(filepath, msg)
+			msg = ""
+		}
+	}
+	newFileName := toTXT(filepath, msg)
+	log.Println("total", sum, "ops into file", newFileName)
+}
+
+// 主要用于物管平台操作日志的转换，根据传入的csv文件路径进行数据转换并写入同名的txt文件中，参数：（csv文件路径，目标数据列的位置）
 func getStateDigit(filepath string, targetidx int) {
-	sum := -1 //用于跳过第一行的表头
-	file, err := os.Open(fmt.Sprintf("./%s", filepath))
+	if !isGoodFormat(filepath) {
+		log.Println(filepath, "错误的目标文件格式...")
+		return
+	}
+	clrTXT_file(filepath) //先清空目标的txt文件
+	sum := -1             //用于跳过第一行的表头
+	file, err := os.Open(filepath)
 	if err != nil {
 		panic(err)
 	}
@@ -98,16 +163,17 @@ func getStateDigit(filepath string, targetidx int) {
 	log.Println("total", sum, "ops into file", newFileName)
 }
 
-// 以追加方式写入文件，并返回新文件名
+// 以追加方式写入文件，文件名为对传入的相对路径改变文件后缀，并返回新文件名
 func toTXT(filename string, content string) string {
-	for i := range filename {
+	dotIndex := 1
+	for i := len(filename) - 1; i >= 0; i-- {
 		if filename[i] == '.' {
-			filename = filename[:i]
+			dotIndex = i
 			break
 		}
 	}
 	// newFileNameFront := filepath.Base(filename)
-	newFileName := "./" + filename + ".txt"
+	newFileName := filename[:dotIndex] + ".txt"
 	// 指定要写入的字符串
 
 	// 以读写模式打开文件，如果文件不存在则创建，0644 是文件权限
@@ -129,4 +195,61 @@ func toTXT(filename string, content string) string {
 		log.Fatalf("failed to flush file: %s", err)
 	}
 	return newFileName
+}
+
+// 预先清空文件同名的txt文件的内容
+func clrTXT_file(filename string) {
+	//转换为同名的txt文件
+	dotIndex := 1
+	for i := len(filename) - 1; i >= 0; i-- {
+		if filename[i] == '.' {
+			dotIndex = i
+			break
+		}
+	}
+	// newFileNameFront := filepath.Base(filename)
+	newFileName := filename[:dotIndex] + ".txt"
+	// 尝试打开文件，如果文件不存在则直接返回
+	file, err := os.OpenFile(newFileName, os.O_WRONLY, 0644)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// 文件不存在，不进行任何操作，直接返回
+			return
+		}
+		// 处理其他打开文件时的错误
+		panic(err)
+	}
+	defer file.Close()
+
+	// 清空文件内容
+	err = file.Truncate(0)
+	if err != nil {
+		// 处理截断文件时的错误
+		panic(err)
+	}
+}
+
+// 将传入的字符串数据不断除10，直到在100范围内
+func convertIn100(dgt string) int {
+	x, err := strconv.Atoi(dgt)
+	if err != nil {
+		panic(err)
+	}
+	for x > 100 {
+		x = x / 10
+	}
+	return x
+}
+
+// 检查传入的文件名后缀是否为csv
+func isGoodFormat(filename string) bool {
+	id := -1
+	for i := len(filename) - 1; i >= 0; i-- {
+		if filename[i] == '.' {
+			id = i
+			break
+		}
+	}
+	tail := filename[id+1:]
+	return tail == "csv"
 }
